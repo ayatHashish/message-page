d7f99ac058fd9ab5708e30013290bc4bbe2409c6
[ayat] <[ahashish@it-blocks.com]>
2024-05-22 14:55:34 +0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// search 
+
 let searchInput =document.getElementById('searchInput')
 let clearIcon =document.getElementById('clearIcon')
 
@@ -15,4 +17,31 @@ clearIcon.addEventListener('click', function() {
     searchInput.value = '';
     this.style.display = 'none';
     searchInput.focus();
+});
+
+
+
+
+let seeMore = document.getElementById('more');
+let seeLess = document.getElementById('less');
+let content = document.getElementById('collapseContent');
+
+seeMore.addEventListener('click', function() {
+    let content = document.getElementById('collapseContent');
+
+    content.classList.add(show)
+    seeMore.style.display= none
+    seeLess.style.display= block
+ 
+});
+
+
+
+seeLess.addEventListener('click', function() {
+let content = document.getElementById('collapseContent');
+
+    content.classList.add(hide)
+    seeLess.style.display= none
+    seeMore.style.display= block
+ 
 });
